98ca08c293ad92328398b86cec848ecee495d5d3
CaioFirme <85518652+clfirme@users.noreply.github.com>
2025-03-09 16:36:29 -0300

    Delete mod1 directory

--- mod1/modPath.js
+++
@@ -1,7 +0,0 @@
-import {basename, dirname, extname} from 'path';
-const filePath = './testeModPath.txt';
-let nomeArquivo = basename(filePath);
-console.log (nomeArquivo);
-console.log(dirname(filePath));
-console.log(extname(filePath));
-
